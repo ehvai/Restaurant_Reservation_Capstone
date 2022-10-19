8a76de924741bff848b5b9bd8370d45212edd44b
Erica <ericaewallbridge@gmail.com>
2022-10-19 12:24:58 -0400

    Updated the seed file and seeded the database.  Updated the NewReservation file.  Added the App.css with css for the reservation file.

--- front-end/src/layout/NewReservation.js
+++ front-end/src/layout/NewReservation.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import { createReservation } from "../utils/api";
+import { Link } from "react-router-dom";
+import "../App.css";
 
 const initialReservation = {
   first_name: "",
@@ -16,8 +18,8 @@ function NewReservation() {
   });
 
   async function addNewReservation() {
-    const abortcontroller = new AbortController();
-    return await createReservation(newReservation, AbortController.signal);
+    const abortController = new AbortController();
+    return await createReservation(newReservation, abortController.signal);
   }
 
   const handleChange = (event) => {
@@ -25,11 +27,6 @@ function NewReservation() {
       ...newReservation,
       [event.target.name]: event.target.value,
     });
-  };
-
-  const handleCancel = (event) => {
-    event.preventDefault();
-    setNewReservation({ ...initialReservation });
   };
 
 
@@ -40,102 +37,125 @@ function NewReservation() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div className="form-group">
-        <div>
-          <label htmlFor="first_name">First Name</label>
-        </div>
-
-        <div>
-          <input
-            name="first_name"
-            id="first_name"
-            onChange={handleChange}
-            value={newReservation.first_name}
-            required={true}
-          />
-        </div>
-      </div>
-      <div className="form-group">
-        <div>
-          <label htmlFor="last_name">Last Name</label>
-        </div>
-        <div>
-          <input
-            name="last_name"
-            id="last_name"
-            onChange={handleChange}
-            value={newReservation.last_name}
-            required={true}
-          />
-        </div>
-      </div>
-      <div className="form-group">
-        <div>
-          <label htmlFor="mobile_number">Mobile Number</label>
-        </div>
-        <div>
-          <input
-            name="mobile_number"
-            id="mobile_number"
-            onChange={handleChange}
-            value={newReservation.mobile_number}
-            required={true}
-          />
-        </div>
-      </div>
-      <div className="form-group">
-        <div>
-          <label htmlFor="reservation_date">Date</label>
-        </div>
-        <div>
-          <input
-            type="date"
-            name="reservation_date"
-            id="reservation_date"
-            onChange={handleChange}
-            value={newReservation.reservation_date}
-            required={true}
-          />
-        </div>
-      </div>
-      <div className="form-group">
-        <div>
-          <label htmlFor="reservation_time">Time</label>
-        </div>
-        <div>
-          <input
-            type="time"
-            name="reservation_time"
-            id="reservation_time"
-            onChange={handleChange}
-            value={newReservation.reservation_time}
-            required={true}
-          />
-        </div>
-      </div>
-      <div className="form-group">
-        <div>
-          <label htmlFor="people">People</label>
-        </div>
-        <div>
-          <input
-            type="integer"
-            name="people"
-            id="people"
-            onChange={handleChange}
-            value={newReservation.people}
-            required={true}
-          />
-        </div>
-      </div>
+    <div>
       <div>
-        <div>
-          <button type="submit" class="btn btn-secondary">Submit</button>
-          <button type="button" class="btn btn-outline-secondary" onClick={handleCancel}>Cancel</button>
-        </div>
+        <h1>Create Reservation</h1>
       </div>
-    </form>
+      <form onSubmit={handleSubmit}>
+        <div className="row createRes">
+          <div className="form-group col">
+            <div>
+              <label htmlFor="first_name">First Name</label>
+            </div>
+            <div>
+              <input
+                name="first_name"
+                id="first_name"
+                onChange={handleChange}
+                value={newReservation.first_name}
+                placeholder="First Name"
+                required={true}
+              />
+            </div>
+          </div>
+          <div className="form-group col">
+            <div>
+              <label htmlFor="last_name">Last Name</label>
+            </div>
+            <div>
+              <input
+                name="last_name"
+                id="last_name"
+                onChange={handleChange}
+                value={newReservation.last_name}
+                placeholder="Last Name"
+                required={true}
+              />
+            </div>
+          </div>
+          <div className="form-group col">
+            <div>
+              <label htmlFor="mobile_number">Mobile Number</label>
+            </div>
+            <div>
+              <input
+                name="mobile_number"
+                id="mobile_number"
+                onChange={handleChange}
+                value={newReservation.mobile_number}
+                placeholder="Mobile Number"
+                required={true}
+              />
+            </div>
+          </div>
+        </div>
+        <div className="row createRes">
+          <div className="form-group col">
+            <div>
+              <label htmlFor="reservation_date">Date</label>
+            </div>
+            <div>
+              <input
+                type="date"
+                name="reservation_date"
+                id="reservation_date"
+                onChange={handleChange}
+                value={newReservation.reservation_date}
+                required={true}
+              />
+            </div>
+          </div>
+          <div className="form-group col">
+            <div>
+              <label htmlFor="reservation_time">Time</label>
+            </div>
+            <div>
+              <input
+                type="time"
+                name="reservation_time"
+                id="reservation_time"
+                onChange={handleChange}
+                value={newReservation.reservation_time}
+                required={true}
+              />
+            </div>
+          </div>
+          <div className="form-group col">
+            <div>
+              <label htmlFor="people">People</label>
+            </div>
+            <div>
+              <input
+                type="integer"
+                name="people"
+                id="people"
+                onChange={handleChange}
+                value={newReservation.people}
+                required
+                min="1"
+              />
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className="row">
+            <div className="col-5">
+              <button type="button" className="btn btn-primary mr-2">
+                <span className="oi oi-check"></span>
+                Submit
+              </button>
+              <Link to={`/dashboard/`}
+                type="cancel"
+                className="btn btn-secondary button"
+              >
+                <span className="oi oi-x"></span>
+                Cancel
+              </Link>
+            </div>
+          </div>
+        </div>
+      </form>
+    </div>
   );
 }
 
